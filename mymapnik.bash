ec91d12117d45d5c0a49e5b48c111311ee1f77e2
rudzick <oliver@rudzick.de>
2021-09-22 02:09:21 +0200

    mymapnik.bash

--- mymapnik.bash
+++ mymapnik.bash
@@ -11,11 +11,13 @@ PROJEKTFILE="project_mymapnik.mml"
 PROJEKTFILEXML="project_mymapnik.xml"
 cp $CARTO_DIR/project.mml Baumsorten_Erweiterung/$PROJEKTFILE
 cd Baumsorten_Erweiterung/
-sed -s "s+Baumsorten_Erweiterung+$CWD/Baumsorten_Erweiterung+g" project_mymapnik.diff > project_mymapnik_cwd.diff
+# Modifizierter amenity-points-Style, der Baumsymbole schon ab Zoomlevel 17 zeigt
+sed -i "s+style/amenity-points.mss+$CWD/Baumsorten_Erweiterung/amenity-points_trees_zoom16.mss+g" project_mymapnik.mml
+sed -s "s+Baumsorten_Erweiterung+$CWD/Baumsorten_Erweiterung+g" my_mms-files.mml > my_mms-files_cwd.mml
+# Einfuegen der zusaetzlichen .mss-Files aus my_mms-files_cwd.mml
+sed -n -i -e '/Layer:/r my_mms-files_cwd.mml' -e 1x -e '2,${x;p}' -e '${x;p}' project_mymapnik.mml
 sed -s "s+Baumsorten_Erweiterung+$CWD+g" project_mymapnik_mypostgis.diff > project_mymapnik_mypostgis_cwd.diff
 cp $CARTO_DIR/style/amenity-points.mss amenity-points_trees_zoom16.mss
-patch -p0 amenity-points_trees_zoom16.mss < amenity_points.diff
-patch -p0 $PROJEKTFILE < project_mymapnik_cwd.diff
 patch -p0 $PROJEKTFILE < project_mymapnik_mypostgis_cwd.diff
 rm project_mymapnik_cwd.diff project_mymapnik_mypostgis_cwd.diff
 cat parzellennummern_layer.mml >> $PROJEKTFILE
@@ -25,7 +27,7 @@ cat strauch_layer.mml >> $PROJEKTFILE
 cat leisure_description_layer.mml >> $PROJEKTFILE
 cp $PROJEKTFILE $CARTO_DIR
 cd $CARTO_DIR
-/usr/local/bin/carto $PROJEKTFILE > $PROJEKTFILEXML
+/usr/bin/carto $PROJEKTFILE > $PROJEKTFILEXML
 mv $PROJEKTFILE $CWD
 echo $CARTO_DIR/$PROJEKTFILEXML
 
