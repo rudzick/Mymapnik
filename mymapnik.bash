2e7840053dcbd15856c2804010d732f71139ac05
rudzick <oliver@rudzick.de>
2022-09-07 09:37:53 +0200

    Modifizierten amenity-points-Style, der Baumsymbole schon ab Zoomlevel 17 zeigt, aus mymapnik.bash entfernt

--- mymapnik.bash
+++ mymapnik.bash
@@ -14,9 +14,9 @@ PROJEKTFILE="project_mymapnik.mml"
 PROJEKTFILEXML="project_mymapnik.xml"
 cp $CARTO_DIR/project.mml Baumsorten_Erweiterung/$PROJEKTFILE
 cd Baumsorten_Erweiterung/
-# Modifizierter amenity-points-Style, der Baumsymbole schon ab Zoomlevel 17 zeigt
-sed -i "s+style/amenity-points.mss+$CWD/Baumsorten_Erweiterung/amenity-points_trees_zoom16.mss+g" project_mymapnik.mml
-sed -s "s+Baumsorten_Erweiterung+$CWD/Baumsorten_Erweiterung+g" my_mms-files.mml > my_mms-files_cwd.mml
+## Modifizierter amenity-points-Style, der Baumsymbole schon ab Zoomlevel 17 zeigt
+#sed -i "s+style/amenity-points.mss+$CWD/Baumsorten_Erweiterung/amenity-points_trees_zoom16.mss+g" project_mymapnik.mml
+#sed -s "s+Baumsorten_Erweiterung+$CWD/Baumsorten_Erweiterung+g" my_mms-files.mml > my_mms-files_cwd.mml
 # Einfuegen der zusaetzlichen .mss-Files aus my_mms-files_cwd.mml
 sed -n -i -e '/Layer:/r my_mms-files_cwd.mml' -e 1x -e '2,${x;p}' -e '${x;p}' project_mymapnik.mml
 sed -s "s+Baumsorten_Erweiterung+$CWD+g" project_mymapnik_mypostgis.diff > project_mymapnik_mypostgis_cwd.diff
